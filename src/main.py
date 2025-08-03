6e76eaf04d66988d3ca67d2177f9a0826fdbe795
Xaitan <utrustad.sprang_0z@icloud.com>
2025-08-03 08:51:55 +0200

    stupid variable change

--- src/main.py
+++ src/main.py
@@ -14,14 +14,14 @@ template_path = "./template.html"
 
 def main():
     print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    if os.path.exists(dir_path_output):
+        shutil.rmtree(dir_path_output)
 
     print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
+    copy_files_recursive(dir_path_static, dir_path_output)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_output, basepath)
 
 
 main()
